d2fd1c4f75447b294457646fd069231e1397ec4b
Luis Angel <laha11590@hotmail.com>
2022-05-24 12:45:10 -0700

    Se creo un CRUD

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+$router->get('/version', function () use ($router) {
     return $router->app->version();
+});
+
+
+$router->get('/saludo',function (){
+    return "Hola mi nombre es Luis Angel";
+});
+
+$router->post('/saludo',function (){
+    return "Se creo un dato";
+});
+
+$router->put('/saludo',function (){
+    return "Se cambio un dato";
+});
+
+$router->delete('/saludo',function (){
+    return "Se elimino un dato";
 });
